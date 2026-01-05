49e9e93f1ecf06f8d44a31b1356817ea26efd599
Datengu <jack.dean1@btinternet.com>
2026-01-05 23:51:25 +0000

    Add API client methods for worker signature

---
+++ Mep1.Erp.Core/WorkerSignatureDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mep1.Erp.Core;
+
+public sealed class WorkerSignatureDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string? SignatureName { get; set; }
+    public DateTime? SignatureCapturedAtUtc { get; set; }
+}
